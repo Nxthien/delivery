350b022e9c8a91b9c1b7d615348ff4e8739513b2
Thien Nguyen <nxthien58@gmail.com>
2016-09-20 00:06:55 +0700

    make release version limit to 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,8 @@ set :deploy_to, '/www/deploy/delivery'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
+
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
